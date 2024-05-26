b6eb15e2b8f36657f5c4a24659722ce8eeb4f640
keanuLapid <keanu@csmbataan.ph.education>
2024-05-27 00:29:33 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CountNumbersWithoutFive(1, 9));     
+        Console.WriteLine(CountNumbersWithoutFive(4, 17));    
+    }
+
+    static int CountNumbersWithoutFive(int start, int end)
+    {
+        int count = 0;
+        for (int i = start; i <= end; i++)
+        {
+            if (!ContainsDigit(i, 5))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static bool ContainsDigit(int number, int digit)
+    {
+        while (number != 0)
+        {
+            if (number % 10 == digit)
+            {
+                return true;
+            }
+            number /= 10;
+        }
+        return false;
+    }
+}
